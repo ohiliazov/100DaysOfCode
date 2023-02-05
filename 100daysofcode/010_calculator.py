cef10e986f3db1a4a70653b6928b65a7139fe01f
Oleksandr Hiliazov <oleksandr.hiliazov@eleks.com>
2023-02-05 16:16:11 +0100

    Day 10

---
+++ 100daysofcode/010_calculator.py
@@ -0,0 +1,69 @@
+import replit
+
+logo = r"""
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+def power(a, b):
+    return a ** b
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+    "^": power,
+}
+
+should_continue = "y"
+
+print(logo)
+x = float(input("What is the first number?: "))
+
+while should_continue.lower() == "y":
+    print("\n".join(operations.keys()))
+    op = input("Pick an operation: ")
+    y = float(input("What is the second number?: "))
+
+    op_func = operations[op]
+    z = op_func(x, y)
+    print(f"{x} {op} {y} = {z}")
+
+    x = z
+
+    should_continue = input(
+        f"Type 'y' to continue calculating with {x} or 'n' to quit.\n"
+    )
+
+    replit.clear()
