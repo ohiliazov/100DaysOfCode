f311c8948b3a74048deb70b40815e0f603e64467
Oleksandr Hiliazov <oleksandr.hiliazov@eleks.com>
2023-02-16 18:26:42 +0100

    Day 18

---
+++ 100daysofcode/018_turtle/__init__.py
@@ -0,0 +1,34 @@
+import random
+
+import colorgram
+from turtle import Turtle, Screen, colormode
+
+colormode(255)
+
+colors = colorgram.extract("hurst-spot-painting.webp", 31)[1:]
+
+
+def random_color():
+    chosen_color = random.choice(colors).rgb
+    return chosen_color.r, chosen_color.g, chosen_color.b
+
+
+t = Turtle()
+t.hideturtle()
+t.penup()
+t.speed("fastest")
+
+t.setx(-250)
+t.sety(-250)
+
+for _ in range(10):
+    for _ in range(10):
+        t.dot(20, random_color())
+        t.forward(50)
+    t.setheading(90)
+    t.forward(50)
+    t.setheading(0)
+    t.back(500)
+
+screen = Screen()
+screen.exitonclick()
