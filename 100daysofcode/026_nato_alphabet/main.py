c7a92704173cd795b5a435b23e11ca157edd7461
Oleksandr Hiliazov <oleksandr.hiliazov@eleks.com>
2023-03-10 17:13:04 +0100

    Day 30

--- 100daysofcode/026_nato_alphabet/main.py
+++ 100daysofcode/026_nato_alphabet/main.py
@@ -4,6 +4,11 @@ df = pd.read_csv("nato_phonetic_alphabet.csv")
 
 nato_alphabet = {letter: word for _, (letter, word) in df.iterrows()}
 
-name = input("Enter a word: ")
+while True:
+    name = input("Enter a word: ")
 
-print([nato_alphabet[letter.upper()] for letter in name])
+    try:
+        print([nato_alphabet[letter.upper()] for letter in name])
+        exit()
+    except KeyError:
+        print("Only letters in the alphabet.")
--- 100daysofcode/029_030_password_manager/main.py
+++ 100daysofcode/029_030_password_manager/main.py
@@ -1,4 +1,4 @@
-import csv
+import json
 import random
 import string
 import tkinter as tk
@@ -21,23 +21,49 @@ def generate_password():
     pyperclip.copy(password)
 
 
+def read_data() -> dict:
+    try:
+        with open("data.json") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+
+
+def search_password():
+    data = read_data()
+
+    website = website_entry.get()
+    if website in data:
+        username = data[website]["username"]
+        password = data[website]["password"]
+        messagebox.showinfo(
+            title=website,
+            message=f"Email: {username}\nPassword: {password}",
+        )
+    else:
+        messagebox.showerror(
+            title="Error",
+            message="Password not found!",
+        )
+
+
 def store_data():
+    data = read_data()
+
     website = website_entry.get().strip()
     username = username_entry.get().strip()
     password = password_entry.get().strip()
 
     if not website or not username or not password:
         messagebox.showerror(title=website, message="Please fill all entries.")
-    else:
-        is_ok = messagebox.askokcancel(title=website, message="Are you sure?")
 
-        if is_ok:
-            with open("data.csv", mode="a") as file:
-                writer = csv.writer(file)
-                writer.writerow([website, username, password])
+    elif messagebox.askokcancel(title=website, message="Are you sure?"):
+        data[website] = {"username": username, "password": password}
+        with open("data.json", mode="w") as file:
+            json.dump(data, file, indent=4)
 
-            website_entry.delete(0, tk.END)
-            password_entry.delete(0, tk.END)
+        website_entry.delete(0, tk.END)
+        password_entry.delete(0, tk.END)
 
 
 window = tk.Tk()
@@ -62,8 +88,8 @@ username_label.grid(row=2, column=0, sticky=tk.E)
 password_label = tk.Label(text="Password:")
 password_label.grid(row=3, column=0, sticky=tk.E)
 
-website_entry = tk.Entry(width=50)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = tk.Entry(width=25)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 
 username_entry = tk.Entry(width=50)
@@ -72,6 +98,13 @@ username_entry.insert(0, "test@gmail.com")
 
 password_entry = tk.Entry(width=25)
 password_entry.grid(row=3, column=1)
+
+search_btn = tk.Button(
+    text="Search",
+    width=20,
+    command=search_password,
+)
+search_btn.grid(row=1, column=2)
 
 generate_password_btn = tk.Button(
     text="Generate Password",
